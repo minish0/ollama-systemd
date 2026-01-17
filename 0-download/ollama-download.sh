12c033f0e87b71c67e5023684fe9b5281feaa4dc
minish <minish0@users.noreply.github.com>
2026-01-17 15:19:21 +0900

    changed url because zst is just archive

--- 0-download/ollama-download.sh
+++ 0-download/ollama-download.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 arch=$(uname -m | sed 's/x86_/amd/;s/aarch64/arm/')
-curl -LO "https://ollama.com/download/ollama-linux-${arch}.tgz"
+curl -LO "https://ollama.com/download/ollama-linux-${arch}.zst"
+# if you want to additional ROCm support, uncomment the following line
+# curl -LO "https://ollama.com/download/ollama-linux-${arch}-rocm.zst"
