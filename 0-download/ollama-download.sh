fede3f76f1c7721f34a3e6cd315fadc99db0c2b7
minish <minish0@users.noreply.github.com>
2026-01-25 19:14:41 +0900

    fixed invalid download url.

--- 0-download/ollama-download.sh
+++ 0-download/ollama-download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 arch=$(uname -m | sed 's/x86_/amd/;s/aarch64/arm/')
-curl -LO "https://ollama.com/download/ollama-linux-${arch}.zst"
+curl -LO "https://ollama.com/download/ollama-linux-${arch}.tar.zst"
 # if you want to additional ROCm support, uncomment the following line
-# curl -LO "https://ollama.com/download/ollama-linux-${arch}-rocm.zst"
+# curl -LO "https://ollama.com/download/ollama-linux-${arch}-rocm.tar.zst"
