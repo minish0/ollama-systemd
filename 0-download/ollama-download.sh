e757347be0c22d78d1614f43de553fbcb1a3b8b8
minish <minish0@users.noreply.github.com>
2026-03-14 19:58:20 +0900

    changed for print what is retrieved with curl.

--- 0-download/ollama-download.sh
+++ 0-download/ollama-download.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 arch=$(uname -m | sed 's/x86_/amd/;s/aarch64/arm/')
-curl -LO "https://ollama.com/download/ollama-linux-${arch}.tar.zst"
-# if you want to additional ROCm support, uncomment the following line
-# curl -LO "https://ollama.com/download/ollama-linux-${arch}-rocm.tar.zst"
+c=$(printf "curl -LO 'https://ollama.com/download/ollama-linux-${arch}.tar.zst'")
+printf "%s\n" "$c"
+eval "$c"
+# if you want to additional ROCm support, uncomment the following lines
+# c=$(printf "curl -LO 'https://ollama.com/download/ollama-linux-%s-rocm.tar.zst'" "$arch")
+# printf "%s\n" "$c"
+# eval "$c"
